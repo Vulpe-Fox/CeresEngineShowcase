eedf0332a0bbb5fe8a31a837975d0e2e7d2d17ab
BuildTools <unconfigured@null.spigotmc.org>
2019-12-16 04:04:54 -0500

    AGain showing how openGL works

--- UltraCeresStation/src/ceresgame/HelloWorld.java
+++ UltraCeresStation/src/ceresgame/HelloWorld.java
@@ -1,60 +1,21 @@
 package ceresgame;
 
-import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
-import org.lwjgl.opengl.ContextAttribs;
 import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.PixelFormat;
+
+import ceresgame.graphics.DisplayUpdater;
 
 public class HelloWorld{
 	
-	private static final int WIDTH = 1080;
-	private static final int HEIGHT = 720;
-	private static final int FPS = 60;
-	
-	public static void createDisplay() {
-		
-		ContextAttribs cA = new ContextAttribs(3,2);
-		cA.withForwardCompatible(true);
-		cA.withProfileCore(true);
-		
-		try {
-			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
-			Display.create(new PixelFormat(), cA);
-		} catch (LWJGLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		GL11.glViewport(0, 0, WIDTH, HEIGHT);
-		GL11.glClearColor(0.6F, 1.0F, 0.0F, 1.0F);
-		
-		
-	}
-	
-	public static void updateDisplay() {
-		
-
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-		Display.sync(FPS);
-		Display.update();
-		
-	}
-	
-	public static void closeDisplay() {
-		Display.destroy();
-	}
 	
 	public static void main(String[] args) {
-		HelloWorld.createDisplay();
+		DisplayUpdater.createDisplay();
 		
 		while(!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_F)) {
-			HelloWorld.updateDisplay();
+			DisplayUpdater.updateDisplay();
 		}
 		
-		HelloWorld.closeDisplay();
+		DisplayUpdater.closeDisplay();
 	}
 	
 }
--- UltraCeresStation/src/ceresgame/graphics/DisplayUpdater.java
+++ UltraCeresStation/src/ceresgame/graphics/DisplayUpdater.java
@@ -5,6 +5,13 @@
  */
 package ceresgame.graphics;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
+
 /**
  *
  * @author carmc9538
@@ -13,18 +20,40 @@ public class DisplayUpdater {
     
     final static int WIDTH = 1080;
     final static int HEIGHT = 720;
-    
-    public void createDisplay(){
-        
-    }
-    
-    public void updateDisplay(){
-        
-    }
-    
-    public void destroyDisplay(){
-        
-    }
+    private static final int FPS = 60;
+	
+	public static void createDisplay() {
+		
+		ContextAttribs cA = new ContextAttribs(3,2);
+		cA.withForwardCompatible(true);
+		cA.withProfileCore(true);
+		
+		try {
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.create(new PixelFormat(), cA);
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		GL11.glViewport(0, 0, WIDTH, HEIGHT);
+		GL11.glClearColor(0.6F, 1.0F, 0.0F, 1.0F);
+		
+		
+	}
+	
+	public static void updateDisplay() {
+		
+
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+		Display.sync(FPS);
+		Display.update();
+		
+	}
+	
+	public static void closeDisplay() {
+		Display.destroy();
+	}
     
     
 }
