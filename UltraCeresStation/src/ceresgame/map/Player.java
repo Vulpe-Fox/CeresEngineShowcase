3d9f754e74e908cd05bedc93ed0bf497024f4436
pintt3963 <pintt3963@HRH-IC0037415.wrdsb.ca>
2019-12-17 09:37:52 -0500

    added player file

---
+++ UltraCeresStation/src/ceresgame/map/Player.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ceresgame.map;
+
+/**
+ *
+ * @author pintt3963
+ */
+public class Player extends GraphicalComponent {
+    
+    private int xPosRaw;
+    private int yPosRaw;
+    private int xPos;
+    private int yPos;
+    
+    private final int zPos = 0;
+    
+    
+}
