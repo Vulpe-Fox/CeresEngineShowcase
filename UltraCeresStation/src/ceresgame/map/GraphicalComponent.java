86d7e3f3a924264b16a46fa554161a8695481ec1
ivary45 <>
2019-12-17 09:24:27 -0500

    Added width and height o graphical components

--- UltraCeresStation/src/ceresgame/map/GraphicalComponent.java
+++ UltraCeresStation/src/ceresgame/map/GraphicalComponent.java
@@ -15,8 +15,47 @@ public class GraphicalComponent {
     private int yPos;
     private int zPos;
     
-    public GraphicalComponent(int LocationX, int LocationY, int LocationZ){
-        
+    private final int width;
+    private final int height;
+    
+    public GraphicalComponent(int xPos, int yPos, int zPos, int width, int height){
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.zPos = zPos;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public int getzPos() {
+        return zPos;
+    }
+
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public void setyPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public void setzPos(int zPos) {
+        this.zPos = zPos;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
     
 }
