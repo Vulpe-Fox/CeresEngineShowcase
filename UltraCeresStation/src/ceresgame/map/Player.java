494c75ee12e6f57e48c230c2845c650ad4672c4b
pintt3963 <pintt3963@HRH-IC0037415.wrdsb.ca>
2019-12-17 10:06:47 -0500

    cleaning up

--- UltraCeresStation/src/ceresgame/map/Player.java
+++ UltraCeresStation/src/ceresgame/map/Player.java
@@ -13,16 +13,9 @@ import org.lwjgl.input.Keyboard;
  */
 public class Player extends GraphicalComponent {
     
-    private int xPosRaw;
-    private int yPosRaw;
-    private int xPos;
-    private int yPos;
-    
-    private final int zPos = 0;
-
     public Player(int xPos, int yPos, int zPos, int width, int height, String imgPath) {
         super(xPos, yPos, zPos, width, height, imgPath);
-        
+        zPos = 0;
     }
     
     private void movement() {
