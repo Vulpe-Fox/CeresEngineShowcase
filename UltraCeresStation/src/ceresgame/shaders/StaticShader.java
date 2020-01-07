7f2d410e3657e6698cfac37b66df21aa21b69ef6
ivary45 <cameroncarmichael@rogers.com>
2020-01-07 12:54:04 -0500

    Create StaticShader.java

---
+++ UltraCeresStation/src/ceresgame/shaders/StaticShader.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ceresgame.shaders;
+
+/**
+ * 
+ * @author carmc9538
+ */
+public class StaticShader extends ShaderType {
+
+  private final String VERTEX_SHADER_FILE = "src/ceresgame/shaders/vertexShader.txt";
+  private final String FRAGMENT_SHADER_FILE = "src/ceresgame/shaders/fragmentShader.txt";
+  
+  public StaticShader(){
+    super(VERTEX_SHADER_FILE, FRAGMENT_SHADER_FILE);
+  }
+  
+  @Override
+  protected void bindAttributes(){
+    super.bindAttribute(0, "position");
+  }
+
+
+}
