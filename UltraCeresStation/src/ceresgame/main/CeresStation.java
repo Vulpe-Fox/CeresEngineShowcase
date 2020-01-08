469244a6ef4bced9b7f30f20388adbcf0522f4b9
ivary45 <cameroncarmichael@rogers.com>
2020-01-08 08:33:49 -0500

    Update CeresStation.java
    Comments

--- UltraCeresStation/src/ceresgame/main/CeresStation.java
+++ UltraCeresStation/src/ceresgame/main/CeresStation.java
@@ -10,17 +10,29 @@ import ceresgame.map.Player;
 import ceresgame.models.RawModel;
 import ceresgame.models.TexturedModel;
 
+/**
+*The main class containing the runner object which contains all the relevant objects, so they can all be connected together (avoiding static issues)
+*
+*/
 public class CeresStation{
 	
     private Player player;
     private Input inputThread;
     private AudioLoop audioThread;
-	//private Camera camera = new Camera(this); //Please feed in a CeresStation object so you can reference the player
+    //private Camera camera = new Camera(this); //Please feed in a CeresStation object so you can reference the player
 	
+    /**
+    *The constructor of the main game object
+    *
+    */
     public CeresStation() {
     	start();
     }
     
+    /**
+    *The method which starts all relevant processes on startup.
+    *
+    */
     public void start() {
     	player = new Player(0, 0, 0, 50, 50, "resources/images/God.png", null);
     	inputThread = new Input(this);
@@ -30,6 +42,10 @@ public class CeresStation{
         audioThread.start();
     }
     
+    /**
+    *The method which closes all relevant processes on program close.
+    *
+    */
     public void close() {
     	inputThread.stop();
     	audioThread.delete();
@@ -52,14 +68,26 @@ public class CeresStation{
 		DisplayUpdater.closeDisplay();
 	}
 	
+	/**
+	*The method which gets the player object
+	*@return The player object
+	*/
 	public Player getPlayer(){
 		return this.player;	
 	}
 	
+	/**
+	*The method which gets the input thread object
+	*@return The input thread object
+	*/
 	public Input getInput() {
 		return this.inputThread;
 	}
 	
+	/**
+	*The method which gets the audio thread object
+	*@return The audio thread object
+	*/
 	public AudioLoop getAudioLoop() {
 		return this.audioThread;
 	}
