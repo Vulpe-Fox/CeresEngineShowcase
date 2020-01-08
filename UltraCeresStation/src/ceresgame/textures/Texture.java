f39117eca02197991baea3998f9e869f7db86d86
ivary45 <cameroncarmichael@rogers.com>
2020-01-08 08:37:54 -0500

    Update Texture.java
    Comments

--- UltraCeresStation/src/ceresgame/textures/Texture.java
+++ UltraCeresStation/src/ceresgame/textures/Texture.java
@@ -1,13 +1,24 @@
 package ceresgame.textures;
 
+/**
+*An object type which contains the identifier of a texture
+*
+*/
 public class Texture {
 
 	private int textureID;
 	
+	/**
+	*The object of a texture which doesn't contain the actual textures, but instead contains the reference to the object
+	*@param texture The id of the texture referenced elsewhere
+	*/
 	public Texture(int texture) {
 		this.textureID = texture;
 	}
-	
+	/**
+	*Gets the id of the texture
+	*@return The integer id of the texture
+	*/
 	public int getID() {
 		return this.textureID;
 	}
