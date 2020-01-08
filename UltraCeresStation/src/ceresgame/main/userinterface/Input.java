99ce217e13a48f26d2106ae64441f364292b8f2c
ivary45 <cameroncarmichael@rogers.com>
2020-01-08 08:28:46 -0500

    Update Input.java
    Comments

--- UltraCeresStation/src/ceresgame/main/userinterface/Input.java
+++ UltraCeresStation/src/ceresgame/main/userinterface/Input.java
@@ -7,21 +7,32 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.lwjgl.input.Keyboard;
 
-
+/**
+*The input class which runs events based on whether different keys are down.
+*@author ivary45, pintt3963
+*/
 public class Input extends Thread{
     
         public boolean running = true;
         private Player player;
         
+        /**
+        *The constructor of the input class which assigns the current player to be the actual player entity
+        *@param game the runner class being input
+        */
         public Input(CeresStation game){
             this.player = game.getPlayer();
         }
     
+        /**
+        *Runs the thread, which loops checking input and waiting for more
+        *
+        */
         @Override
         public void run(){
             while(running){
                 keyPressed();
-                
+                //TODO change input scheme to reflect delta time changes instead of waiting
                 try {
                     sleep(100);
                 } catch (InterruptedException ex) {
@@ -30,6 +41,10 @@ public class Input extends Thread{
             }
         }
 
+        /**
+        *The method which checks which keys are down and moves the player using said inputs
+        *
+        */
         public void keyPressed() {
             if(Keyboard.isKeyDown(Keyboard.KEY_W)){
                 player.movement(Direction.UP);
